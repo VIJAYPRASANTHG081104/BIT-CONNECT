2256091c3efa80b6586fba43cef8fbb31e5ce2e9
VIJAYPRASANTH08112004 <vijayprasanth08112004@gmail.com>
2024-11-17 22:28:35 +0530

    post completed

--- frontend/src/components/post/PostMenu.jsx
+++ frontend/src/components/post/PostMenu.jsx
@@ -43,7 +43,6 @@ const PostMenu = ({
   };
   return (
     <ul className="post_menu" ref={menu}>
-      {test && <MenuItem icon="pin_icon" title="Pin Post" />}
       <div onClick={() => saveHandler()}>
         {checkSaved ? (
           <MenuItem
@@ -60,13 +59,7 @@ const PostMenu = ({
         )}
       </div>
       <div className="line"></div>
-      {test && <MenuItem icon="edit_icon" title="Edit Post" />}
-      {!test && (
-        <MenuItem
-          icon="turnOnNotification_icon"
-          title="Turn on notifications for this post"
-        />
-      )}
+    
       {imagesLength && (
         <div onClick={() => downloadImages()}>
           <MenuItem icon="download_icon" title="Download" />
@@ -75,19 +68,7 @@ const PostMenu = ({
       {imagesLength && (
         <MenuItem icon="fullscreen_icon" title="Enter Fullscreen" />
       )}
-      {test && <MenuItem img="../../../icons/lock.png" title="Edit audience" />}
-      {test && (
-        <MenuItem
-          icon="turnOffNotifications_icon"
-          title="Turn off notifications for this post"
-        />
-      )}
-      {test && <MenuItem icon="delete_icon" title="Turn off translations" />}
-      {test && <MenuItem icon="date_icon" title="Edit Date" />}
-      {test && (
-        <MenuItem icon="refresh_icon" title="Refresh share attachment" />
-      )}
-      {test && <MenuItem icon="archive_icon" title="Move to archive" />}
+      
       {test && (
         <div onClick={deleteHandler}>
           <MenuItem
@@ -98,13 +79,6 @@ const PostMenu = ({
         </div>
       )}
       {!test && <div className="line"></div>}
-      {!test && (
-        <MenuItem
-          img="../../../icons/report.png"
-          title="Report post"
-          subtitle="i'm concerned about this post"
-        />
-      )}
     </ul>
   );
 };
